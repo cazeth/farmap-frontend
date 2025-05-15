626feefaaaf60a9034116b4b21a60667c9a55c82
cazeth <50043106+cazeth@users.noreply.github.com>
2025-05-15 07:36:11 +0200

    add tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite"
 export default defineNuxtConfig({
 	runtimeConfig: {
 		apiAuthToken: process.env.API_AUTH_TOKEN,
@@ -6,5 +7,11 @@ export default defineNuxtConfig({
 	},
 	compatibilityDate: '2024-11-01',
 	devtools: { enabled: true },
+	css: ['./assets/css/main.css'],
+	vite: {
+		plugins: [
+			tailwindcss(),
+		],
+	},
 	modules: ['@nuxt/eslint', '@nuxt/test-utils']
 })
