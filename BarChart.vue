f2b6ab3c7b0a3b4903c678e391adfa3aa3b1a1c9
cazeth <50043106+cazeth@users.noreply.github.com>
2025-05-15 12:08:06 +0200

    add current year toggle

--- BarChart.vue
+++ BarChart.vue
@@ -1,19 +1,20 @@
 <template>
 	<div class="p-4">
-		<Bar :data="chartData" :options="chartOptions" />
+		<Bar :data="shownChartData" :options="chartOptions" />
 	</div>
 </template>
 
 <script setup lang="ts">
 
 import { Bar } from 'vue-chartjs'
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js';
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 const props = defineProps({
 	isDistribution: Boolean,
 	api_endpoint: String,
+	currentYear: Boolean,
 })
 
 type DataItem = { date: string, spam: number, maybe: number, nonspam: number };
@@ -29,16 +30,42 @@ if (props.isDistribution === true) {
 let spam: number[] = data.value?.map(item => item.spam)!;
 let maybe: number[] = data.value?.map(item => item.maybe)!;
 let ham: number[] = data.value?.map(item => item.nonspam)!;
-let labels: string[] = data.value?.map(item => item.date)!;
-
+let labels: Date[] = data.value?.map(item => new Date(item.date))!;
 
 let chartData = {
-	labels: labels, datasets: [
+	labels: labels.map(item => item.getFullYear().toString() + "/" + (item.getMonth() + 1).toString()), datasets: [
 		{ label: "ham", data: ham, backgroundColor: "#126E82" },
 		{ label: "maybe", data: maybe, backgroundColor: "#CDE8E5" },
 		{ label: "spam", data: spam, backgroundColor: "#A7D0CD" }
 	]
 };
+
+/// calculate stuff for CY data.
+const currentYear = new Date().getFullYear();
+
+let cyData = data.value!.filter((item) => {
+	let date = new Date(item.date);
+	return date.getFullYear() === currentYear;
+})
+
+let cySpam: number[] = cyData.map(item => item.spam)!;
+let cyMaybe: number[] = cyData.map(item => item.maybe)!;
+let cyHam: number[] = cyData.map(item => item.nonspam)!;
+let cyLabels: Date[] = cyData.map(item => new Date(item.date))!;
+
+let cyChartData = {
+	labels: cyLabels.map(item => item.getFullYear().toString() + "/" + (item.getMonth() + 1).toString()), datasets: [
+		{ label: "ham", data: cyHam, backgroundColor: "#126E82" },
+		{ label: "maybe", data: cyMaybe, backgroundColor: "#CDE8E5" },
+		{ label: "spam", data: cySpam, backgroundColor: "#A7D0CD" }
+	]
+}
+
+
+const shownChartData = computed(() =>
+	props.currentYear ? cyChartData : chartData
+);
+
 
 const chartOptions = ref({
 	type: 'bar',
@@ -51,7 +78,7 @@ const chartOptions = ref({
 		},
 		x: {
 			stacked: true,
-		}
+		},
 	}
 });
 
--- app.vue
+++ app.vue
@@ -1,13 +1,15 @@
 <template>
+	<button @click="currentYear = !currentYear"> Current Year Only </button>
 
 	<div class="grid grid-cols-1 md:grid-cols-2">
-		<BarChart api_endpoint="weekly_spam_scores_counts" :is-distribution="false" />
-		<BarChart api_endpoint="weekly_spam_scores" :is-distribution="true" />
+		<BarChart api_endpoint="weekly_spam_scores_counts" :is-distribution="false"
+			:current-year="currentYear" />
+		<BarChart api_endpoint="weekly_spam_scores" :is-distribution="true" :current-year="currentYear" />
 	</div>
 </template>
 
 <script setup lang="ts">
 
 import BarChart from './BarChart.vue';
-
+let currentYear = ref(true);
 </script>
